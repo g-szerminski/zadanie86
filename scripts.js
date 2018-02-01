ba2035a98b1023659537d89a955897c7f5646b85
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-01 18:09:29 +0100

    Change scripts.js content

--- scripts.js
+++ scripts.js
@@ -1,19 +1,16 @@
 var a = prompt('Podaj wartość a');
 var b = prompt('Podaj wartość b');
-	value = (a * a) - (2 * a * b) - (b * b);
+var	value = (a * a) - (2 * a * b) - (b * b);
 console.log ('The result of the action with variable a: ' + a + ' and with variable b: ' + b + ' is equal to: ' + value);
-
 if (value > 0)	{
 	alert('Wynik dodatni = ' + value);
 	console.log ('Wynik dodatni');
-}
-
-if (value < 0) {
+} 
+else if (value < 0) {
 	alert('Wynik ujemny = ' + value);
 	console.log ('Wynik ujemny');
-}
-
-if (value == 0) {
+} 
+else if (value == 0) {
 	alert('Wynik równy ' + value);
 	console.log ('Wynik = 0');
 }
