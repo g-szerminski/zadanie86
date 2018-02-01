bb4734852a29d969c10ec9350fe17ec8cb43f6eb
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-01 13:32:08 +0100

    Remove comented code from js file

--- scripts.js
+++ scripts.js
@@ -17,19 +17,3 @@ if (value == 0) {
 	alert('Wynik równy ' + value);
 	console.log ('Wynik = 0');
 }
-
-
-
-//alert('Wynik, ' + value);	
-
-
-
-
-
-
-
-/*var a = prompt('Podaj a trójkąta');
-	h = prompt('Podaj h trójkąta');
-	triangleArea = a * h/2;
-alert('Pole trójkąta, ' + triangleArea);
-console.log ('Triangle field with a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);*/
